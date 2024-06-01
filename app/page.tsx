cc1fb9a70e2eafb6076b0dc7013d0f1bdcafcdeb
Luca Fumagalli <fumagalli.lcu@gmail.com>
2024-06-01 10:34:07 +0200

    added login/logout

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,28 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Link from "next/link";
 import { createClient } from "@/utils/supabase/client";
+import { redirect } from "next/navigation";
+import AuthButton from "@/components/AuthButton";
 
 export default async function Home() {
 
   const supabase = createClient();
-  const { data: { user } } = await supabase.auth.getUser()
+  const { data: { user } } = await supabase.auth.getUser();
+
+  const signOut = async () => {
+    "use server";
+
+    const supabase = createClient();
+    await supabase.auth.signOut();
+    return redirect("/login");
+  };
 
   return (
     <div className="">
       <MaxWidthWrapper className="pt-20 flex flex-col">
         <div>
           <h1>Keep track of <span className="bg-primary text-neutral">food expiry</span> dates</h1>
-          {user ? (
-            <h2> Hy {user.email}</h2>
-          ) : (
-            <Link href="/login"
-              className="btn btn-default">
-              Login
-            </Link>
-          )}
+          <AuthButton/>
         </div>
       </MaxWidthWrapper>
     </div>
